b2e3642e7fd3076daa356ec4fc61e4a60fb40c46
0tt00t <orzmeng@ucdavis.edu>
2024-06-28 08:36:25 +0800

    Basic function depolyed

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const indexRouter = require('./routes/index');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const mongoose = require('mongoose');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/', indexRouter);
 
 const PORT = process.env.PORT || 3000;
@@ -29,3 +33,10 @@ io.on('connection', (socket) => {
 });
 
 module.exports = app;
+
+mongoose.connect('mongodb://127.0.0.1:27017/fse_chat_app', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const Message = require('../models/Message');
 
 router.get('/', (req, res) => {
     res.render('index');
+});
+
+router.post('/register', async(req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = new User({ username, password });
+        await user.save();
+        res.redirect(`/chat?username=${username}`);
+    } catch (error) {
+        console.error('User registration failed:', error);
+        res.status(400).send('User registerd fail');
+    }
+});
+
+router.post('/login', async(req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username, password });
+        if (user) {
+            res.redirect(`/chat?username=${username}`);
+        } else {
+            console.error('User login failed:', error);
+            res.status(400).send('Invalid username or password');
+        }
+    } catch (error) {
+        res.status(400).send('User login failed');
+    }
+});
+
+router.get('/chat', async(req, res) => {
+    const username = req.query.username;
+    const messages = await Message.find({});
+    res.render('chat', { username, messages });
+});
+
+router.get('/message', async(req, res) => {
+    try {
+        const message = await Message.find({});
+        res.json(message);
+    } catch (error) {
+        console.error('Error fetching messages:', error);
+        res.status(500).send('Error fetching messages');
+    }
+});
+
+router.post('/messages', async(req, res) => {
+    const { username, message } = req.body;
+    try {
+        const newMessage = new Message({ username, message });
+        await newMessage.save();
+        res.status(200).send(newMessage);
+    } catch (error) {
+        console.error('Error sending message:', error);
+        res.status(500).send('Error sending message');
+    }
 });
 
 module.exports = router;
